7d3d15bb511fc35bfa7e483f019ec6af76df9618
fomentia <isaac@thewilliams.ws>
2017-03-28 17:02:16 -0400

    Move Beachball out of module

--- lib/beachball.rb
+++ lib/beachball.rb
@@ -1,26 +1,22 @@
-require "beachball/version"
+class Beachball
+  def initialize(fps)
+    @delay = 1.0/fps.to_f
+    @frames = %w{ | / - \\ }
+    @iter = 0
+  end
 
-module Beachball
-  class Beachball
-    def initialize(fps)
-      @delay = 1.0/fps.to_f
-      @frames = %w{ | / - \\ }
-      @iter = 0
-    end
-
-    def start
-      @beachball = Thread.new do
-        while @iter do
-          print @frames[(@iter+=1) % @frames.length]
-          sleep @delay
-          print "\b"
-        end
+  def start
+    @beachball = Thread.new do
+      while @iter do
+        print @frames[(@iter+=1) % @frames.length]
+        sleep @delay
+        print "\b"
       end
     end
+  end
 
-    def stop
-      @iter = false
-      @beachball.join
-    end
+  def stop
+    @iter = false
+    @beachball.join
   end
 end
